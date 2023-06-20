2d601a914468dc1b1ef99ce98e6eede8e97c099b
adam-paul <notadampaul@protonmail.com>
2023-06-19 21:13:05 -0700

    Adding all the URLs for all sports

--- src/App.js
+++ src/App.js
@@ -8,14 +8,27 @@ const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<SportsGrid />} />
         <Route path="/nhl" element={<Dashboard sportName="NHL" />} />
         <Route path="/nhl/season" element={<Dashboard sportName="NHL" />} />
         <Route path="/nhl/team" element={<Dashboard sportName="NHL" />} />
         <Route path="/nhl/player" element={<Dashboard sportName="NHL" />} />
         <Route path="/mlb" element={<Dashboard sportName="MLB" />} />
+        <Route path="/mlb/season" element={<Dashboard sportName="MLB" />} />
+        <Route path="/mlb/team" element={<Dashboard sportName="MLB" />} />
+        <Route path="/mlb/player" element={<Dashboard sportName="MLB" />} />
         <Route path="/nba" element={<Dashboard sportName="NBA" />} />
+        <Route path="/nba/season" element={<Dashboard sportName="NBA" />} />
+        <Route path="/nba/team" element={<Dashboard sportName="NBA" />} />
+        <Route path="/nba/player" element={<Dashboard sportName="NBA" />} />
         <Route path="/nfl" element={<Dashboard sportName="NFL" />} />
-        <Route path="/" element={<SportsGrid />} />
+        <Route path="/nfl/season" element={<Dashboard sportName="NFL" />} />
+        <Route path="/nfl/team" element={<Dashboard sportName="NFL" />} />
+        <Route path="/nfl/player" element={<Dashboard sportName="NFL" />} />
+        <Route path="/nhl" element={<Dashboard sportName="NHL" />} />
+        <Route path="/nhl/season" element={<Dashboard sportName="NHL" />} />
+        <Route path="/nhl/team" element={<Dashboard sportName="NHL" />} />
+        <Route path="/nhl/player" element={<Dashboard sportName="NHL" />} />
       </Routes>
     </Router>
   );
