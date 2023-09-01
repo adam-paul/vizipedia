7c9faf86e4bae331f5ec6639676ad48a86249e5b
adam-paul <notadampaul@protonmail.com>
2023-08-31 21:26:32 -0700

    season dashboard updates

--- src/components/SeasonDashboard.js
+++ src/components/SeasonDashboard.js
@@ -1,10 +1,33 @@
 // SeasonDashboard.js
-const SeasonDashboard = () => {
+import React, { useState, useEffect } from 'react';
+
+const SeasonDashboard = ({ sportName }) => {
+    const [selectedSeason, setSelectedSeason] = useState('20182019'); // Default value
+    const [teamData, setTeamData] = useState([]);
+
+    // Function to handle season selection
+    const handleSeasonChange = (event) => {
+        setSelectedSeason(event.target.value);
+    };
+
+    // Fetch data for the selected season
+    useEffect(() => {
+        fetch(`/api/${sportName.toLowerCase()}/team/?season=${selectedSeason}`)
+        .then(response => response.json())
+        .then(data => setTeamData(data))
+        .catch(error => console.log('There was an error fetching data:', error));
+    }, [selectedSeason, sportName]); // Re-run this effect when selectedSeason or sportName changes
+
     return (
       <div>
-        <h2>Season Dashboard</h2>
-        {/* Include season-specific visualizations */}
-        {/* ... */}
+        <h2></h2>
+        {/* Include team-specific visualizations */}
+        {/* Dropdown to select the season */}
+        <select value={selectedSeason} onChange={handleSeasonChange}>
+            <option value="20182019">2018-2019</option>
+            <option value="20192020">2019-2020</option>
+            {/* ... other options ... */}
+        </select>
       </div>
     );
   };
--- src/components/TeamDashboard.js
+++ src/components/TeamDashboard.js
@@ -1,35 +1,12 @@
 // TeamDashboard.js
-import React, { useState, useEffect } from 'react';
-
-const TeamDashboard = ({ sportName }) => {
-    const [selectedSeason, setSelectedSeason] = useState('20182019'); // Default value
-    const [teamData, setTeamData] = useState([]);
-
-    // Function to handle season selection
-    const handleSeasonChange = (event) => {
-        setSelectedSeason(event.target.value);
-    };
-
-    // Fetch team data for the selected season
-    useEffect(() => {
-        fetch(`/api/${sportName.toLowerCase()}/team/?season=${selectedSeason}`)
-        .then(response => response.json())
-        .then(data => setTeamData(data))
-        .catch(error => console.log('There was an error fetching data:', error));
-    }, [selectedSeason, sportName]); // Re-run this effect when selectedSeason or sportName changes
-
-    return (
-      <div>
-        <h2></h2>
-        {/* Include team-specific visualizations */}
-        {/* Dropdown to select the season */}
-        <select value={selectedSeason} onChange={handleSeasonChange}>
-            <option value="20182019">2018-2019</option>
-            <option value="20192020">2019-2020</option>
-            {/* ... other options ... */}
-        </select>
-      </div>
-    );
-  };
+const TeamDashboard = () => {
+  return (
+    <div>
+      <h2>Team Dashboard</h2>
+      {/* Include team-specific visualizations */}
+      {/* ... */}
+    </div>
+  );
+};
 
 export default TeamDashboard;
