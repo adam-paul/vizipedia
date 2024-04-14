9145f7f4c4c891ff8591690df354560a8204bec5
adam-paul <notadampaul@protonmail.com>
2024-04-14 14:18:29 -0700

    trying new path

--- backend/vizipedia/settings/base.py
+++ backend/vizipedia/settings/base.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = "static/"
 
-REACT_APP_DIR = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'frontend')
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
