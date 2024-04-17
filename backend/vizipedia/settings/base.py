fa818fa2baf709ee28eb4800be437c9a487ed718
adam-paul <notadampaul@protonmail.com>
2024-04-17 10:02:39 -0700

    total directory structure reorg AGAIN

--- backend/vizipedia/settings/base.py
+++ backend/vizipedia/settings/base.py
@@ -122,7 +122,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = "static/"
 
-REACT_APP_DIR = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'frontend')
+REACT_APP_DIR = os.path.dirname(os.path.dirname(BASE_DIR))
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
